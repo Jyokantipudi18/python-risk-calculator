b694131869d43a4097aaf8bce0b326531d527c37
Jyothirmai Kantipudi <kantipudijyothirmai@gmail.com>
2025-09-17 15:48:14 -0500

    first commit

---
+++ risk_calculator.py
@@ -0,0 +1,4 @@
+#Simple portifolio risk calculator which calculates total value and expected potential risk.
+if __name__=="__main__":
+    print("Welcome to the simple portfolio risk calculator!")
+    
